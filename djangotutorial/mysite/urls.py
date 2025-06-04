ea89e90baf51af9193b58d61f24daef6d5aefd4a
novakvova <novakvova@gmail.com>
2025-06-04 20:34:37 +0300

    Add static

--- djangotutorial/mysite/urls.py
+++ djangotutorial/mysite/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
 ]
