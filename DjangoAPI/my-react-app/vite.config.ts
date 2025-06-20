b325e34b380ddf48598ec86b2b7484a86967dc80
novakvova <novakvova@gmail.com>
2025-06-20 18:48:37 +0300

    Remove Flowbite

--- DjangoAPI/my-react-app/vite.config.ts
+++ DjangoAPI/my-react-app/vite.config.ts
@@ -1,7 +1,6 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import flowbiteReact from "flowbite-react/plugin/vite";
 import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
@@ -9,7 +8,6 @@ export default defineConfig({
     plugins: [
         react(),
         tailwindcss(),
-        flowbiteReact(),
         svgr({
             svgrOptions: {
                 icon: true,
